bdaf4b9d13d88a2f2f86a23a7b839de513c6ed4d
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2023-07-26 18:04:17 +0530

    Practice48 modify

--- Practice_set/Practice48.java
+++ Practice_set/Practice48.java
@@ -5,15 +5,23 @@ import java.util.*;
         public class Practice48 {
                 public static void main(String args[]) {
                     Scanner sc = new Scanner(System.in);
-                    for(int i = 1 ; i>=1 ; i++) {
-                    System.out.println("Enter the " +i+" number");
+//                     for(int i = 1 ; i>=1 ; i++) {
+//                     System.out.println("Enter the " +i+" number");
+//                             int num = sc.nextInt();
+//                                 if(num % 10 == 0) {
+//                                  break;
+//                                 } else {
+//                                  System.out.println(num);
+//                                 }
+//                     }
+                    do {
+                            System.out.println("Enter the number");
                             int num = sc.nextInt();
-                                if(num % 10 == 0) {
-                                 break;
-                                } else {
-                                 System.out.println(num);
-                                }
-                    }
+                            if(num % 10 == 0) {
+                             break;
+                            }
+                            System.out.println(num);
+                    } while(true) ;
                 System.out.println("You have entered the multiple of 10");
                 }
         }
