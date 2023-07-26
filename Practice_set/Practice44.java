2c81afffa6b5ede24838cb30fd4ec7480ed5308a
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2023-07-26 09:34:51 +0530

    Practice44.java add (For Loop)

---
+++ Practice_set/Practice44.java
@@ -0,0 +1,23 @@
+//  Print square pattern
+/*     *  *  *  *
+       *  *  *  *
+       *  *  *  *
+       *  *  *  *
+*/
+import java.util.*;
+
+   public class Practice44 {
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number :");
+            int num = 4;
+            for(int i = 0 ; i < num ; i++) {
+                    System.out.println("*  *  *  *");
+            }
+//             while(num > 0) {
+//             System.out.println("*  *  *  *");
+//             num--;
+//             }
+
+        }
+   }
